0e1691181cadd86ebd15ae5262c0dbd1fdea3372
Bipina Poudel <bipina234@gmail.com>
2022-03-28 03:11:20 +0545

    Project using relative layout get added

---
+++ HelloRelative/app/src/main/java/me/Bipina/hellorelative/SecondActivity.java
@@ -0,0 +1,15 @@
+package me.Bipina.hellorelative;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.mahakagg.hellorelative.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
